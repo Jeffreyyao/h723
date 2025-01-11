0302099519ad7a8620523432598e611c6c2709d1
jeffrey-yaozx <jeffrey@hestia.kitchen>
2025-01-11 09:37:35 +0800

    formatting

--- mlx90640/MLX90640_I2C_Driver.c
+++ mlx90640/MLX90640_I2C_Driver.c
@@ -14,70 +14,64 @@
  * limitations under the License.
  *
  */
-#include <stdio.h>
 #include "MLX90640_I2C_Driver.h"
 
-const I2CConfig i2c_cfg = {
-  .timingr = STM32_TIMINGR_PRESC(2) |
-      STM32_TIMINGR_SCLDEL(9U) | STM32_TIMINGR_SDADEL(0U) |
-      STM32_TIMINGR_SCLH(112U)  | STM32_TIMINGR_SCLL(162U),
-  .cr1 = 0,
-  .cr2 = 0
-};
+#include <stdio.h>
 
-void MLX90640_I2CInit()
-{   
-	i2cStart(&I2CD4, &i2c_cfg);
+const I2CConfig i2c_cfg = {
+    .timingr = STM32_TIMINGR_PRESC(2) | STM32_TIMINGR_SCLDEL(9U) |
+               STM32_TIMINGR_SDADEL(0U) | STM32_TIMINGR_SCLH(112U) |
+               STM32_TIMINGR_SCLL(162U),
+    .cr1 = 0,
+    .cr2 = 0};
+
+void MLX90640_I2CInit() { i2cStart(&I2CD4, &i2c_cfg); }
+
+int MLX90640_I2CRead(uint8_t slaveAddr, uint16_t startAddress,
+                     uint16_t nMemAddressRead, uint16_t *data) {
+  uint8_t *p = (uint8_t *)data;
+  int cnt = 0;
+
+  // ack = HAL_I2C_Mem_Read(&hi2c1, (slaveAddr<<1), startAddress,
+  // I2C_MEMADD_SIZE_16BIT, p, nMemAddressRead*2, 500);
+  uint8_t tx_buf[] = {startAddress >> 8, startAddress & 0xFF};
+  msg_t ack = i2cMasterTransmitTimeout(&I2CD4, slaveAddr, tx_buf, 2, p,
+                                       nMemAddressRead * 2, TIME_MS2I(50));
+
+  if (ack != MSG_OK) {
+    return -1;
+  }
+
+  for (cnt = 0; cnt < nMemAddressRead * 2; cnt += 2) {
+    uint8_t tempBuffer = p[cnt + 1];
+    p[cnt + 1] = p[cnt];
+    p[cnt] = tempBuffer;
+  }
+
+  return 0;
 }
 
+int MLX90640_I2CWrite(uint8_t slaveAddr, uint16_t writeAddress, uint16_t data) {
+  uint8_t cmd[2];
+  static uint16_t dataCheck;
 
-int MLX90640_I2CRead(uint8_t slaveAddr, uint16_t startAddress, uint16_t nMemAddressRead, uint16_t *data)
-{
-  uint8_t *p = (uint8_t *)data;
-	int cnt = 0;
-	
-	// ack = HAL_I2C_Mem_Read(&hi2c1, (slaveAddr<<1), startAddress, I2C_MEMADD_SIZE_16BIT, p, nMemAddressRead*2, 500);
-  uint8_t tx_buf[] = {startAddress >> 8, startAddress & 0xFF};
-  msg_t ack = i2cMasterTransmitTimeout(&I2CD4, slaveAddr, tx_buf, 2, p, nMemAddressRead*2, TIME_MS2I(50));
+  cmd[0] = data >> 8;
+  cmd[1] = data & 0x00FF;
 
-	if (ack != MSG_OK) 
-  {
-		return -1;
-	}
+  // ack = HAL_I2C_Mem_Write(&hi2c1, sa, writeAddress, I2C_MEMADD_SIZE_16BIT,
+  // cmd, sizeof(cmd), 500);
+  msg_t ack = i2cMasterTransmitTimeout(&I2CD4, slaveAddr, cmd, 2, NULL, 0,
+                                       TIME_MS2I(50));
 
-	for(cnt=0; cnt < nMemAddressRead*2; cnt+=2) {
-		uint8_t tempBuffer = p[cnt+1];
-		p[cnt+1] = p[cnt];
-		p[cnt] = tempBuffer;
-	}
+  if (ack != MSG_OK) {
+    return -1;
+  }
 
-	return 0;   
-} 
+  MLX90640_I2CRead(slaveAddr, writeAddress, 1, &dataCheck);
 
+  if (dataCheck != data) {
+    return -2;
+  }
 
-int MLX90640_I2CWrite(uint8_t slaveAddr, uint16_t writeAddress, uint16_t data)
-{
-	uint8_t cmd[2];
-	static uint16_t dataCheck;
-
-	cmd[0] = data >> 8;
-	cmd[1] = data & 0x00FF;
-
-
-	// ack = HAL_I2C_Mem_Write(&hi2c1, sa, writeAddress, I2C_MEMADD_SIZE_16BIT, cmd, sizeof(cmd), 500);
-  msg_t ack = i2cMasterTransmitTimeout(&I2CD4, slaveAddr, cmd, 2, NULL, 0, TIME_MS2I(50));
-
-	if (ack != MSG_OK)
-	{
-		return -1;
-	}         
-	
-	MLX90640_I2CRead(slaveAddr, writeAddress, 1, &dataCheck);
-	
-	if (dataCheck != data)
-	{
-		return -2;
-	}    
-	
-	return 0;
+  return 0;
 }
